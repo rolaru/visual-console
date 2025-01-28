5bd7f15cabeead0f7c0ecb0d8ac5efaeec522fa2
Olaru Razvan-Catalin <olarurazvan_2000@yahoo.com>
2025-01-28 23:11:51 +0200

    Allow writing more messages on the same line
    Implement a boolean "useLineBreak" parameter for logMessage() that allows to write logged messages on the same line.

--- logging.js
+++ logging.js
@@ -6,26 +6,54 @@ const consoleConfig = {
 };
 
 let currentLine = 1;
+let noLineBreakBefore = false;
 
 if (consoleConfig.allowHorizontalScroll) {
   consoleElement.classList.add('with-horizontal-scroll');
 }
 
-export const logMessage = (message) => {
+export const logMessage = (message, useLineBreak = true) => {
+  const spacing = useLineBreak ? '<br />' : ' ';
+
   if (message) {
     message = JSON.stringify(message, null, '  ');
     // Remove start and trailing " characters added by JSON.stringify()
     if (message[0] === '"' && message[message.length - 1] === '"') {
       message = message.substring(1, message.length - 1);
     }
-    message += '<br />';
+    message += spacing;
   } else {
-    message = '<br />';
+    message = spacing;
   }
 
-  const lineNumber = consoleConfig.withLineNumbers
-    ? `<span class="line-number">${currentLine++}</span>`
-    : '';
+  let lineNumber = '';
+  if (consoleConfig.withLineNumbers) {
+    /*
+      Replace all new lines from the message formatted with
+      JSON.stringify() with <br / > + line number spans.
+    */
+    while (message.indexOf('\n') > -1) {
+      message = message.replace(
+        '\n',
+        `<br /><span class="line-number">${currentLine++}</span>`
+      );
+    }
+
+    // Put a line number for each message that starts on a new line
+    if (useLineBreak && !noLineBreakBefore) {
+      lineNumber = `<span class="line-number">${currentLine++}</span>`;
+
+      // Start in-line logging
+    } else if (!useLineBreak && !noLineBreakBefore) {
+      noLineBreakBefore = true;
+      lineNumber = `<span class="line-number">${currentLine++}</span>`;
+
+      // Finish in-line logging
+    } else if (useLineBreak && noLineBreakBefore) {
+      noLineBreakBefore = false;
+      lineNumber = `<br /><span class="line-number">${currentLine++}</span>`;
+    }
+  }
 
   consoleElement.innerHTML += `${lineNumber}${message}`;
 };
